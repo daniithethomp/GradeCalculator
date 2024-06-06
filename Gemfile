e9fa1b49c337ae65616434c2f63dc2592ed8aac5
daniithompson <danii.thompson8@gmail.com>
2024-06-06 17:52:48 +0100

    added user login system, need to add views

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source "https://rubygems.org"
+source "http://rubygems.org"
 
 ruby "3.1.2"
 
@@ -33,7 +33,7 @@ gem "jbuilder"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+ gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "/home", to: "sessions#home"
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#logout"
+  post "/logout", to: "sessions#destroy"
+
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +14,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "sessions#home"
 end
