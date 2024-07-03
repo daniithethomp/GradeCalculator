c0f616649154e73d2c6066f5f1bd2aba50203a45
daniithompson <danii.thompson8@gmail.com>
2024-07-03 23:35:50 +0100

    added helper methods

--- app/helpers/course_modules_helper.rb
+++ app/helpers/course_modules_helper.rb
@@ -1,2 +1,13 @@
 module CourseModulesHelper
+    def get_module_tests(m)
+        return Test.where(course_module_id: m.id)
+    end
+
+    def get_module_score(m)
+        module_tests = helpers.get_module_tests(m)
+        total_score = 0
+        module_tests.each do |t|
+            total_score += helpers.get_test_score(t)
+        end
+    end
 end
--- app/helpers/years_helper.rb
+++ app/helpers/years_helper.rb
@@ -1,2 +1,13 @@
 module YearsHelper
+    def get_year_modules(year)
+        return CourseModule.where(year_id: year.id)
+    end
+
+    def get_total_year_credits(year)
+        total_credits = 0
+        get_year_modules(year).each do |m|
+            total_credits += m.credits
+        end
+        return total_credits
+    end
 end
