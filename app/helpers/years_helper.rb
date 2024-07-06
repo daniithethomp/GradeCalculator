9806c8c47e354ec1bfa2ceef5faca351f4057b3c
daniithompson <danii.thompson8@gmail.com>
2024-07-06 19:08:49 +0100

    styling changes

--- app/helpers/years_helper.rb
+++ app/helpers/years_helper.rb
@@ -10,4 +10,15 @@ module YearsHelper
         end
         return total_credits
     end
+
+    def get_year_score(year)
+        total_score = 0
+        total_credits = get_total_year_credits(year)
+        modules = get_year_modules(year)
+        modules.each do |m|
+            score = get_module_score(m)
+            total_score += score * (m.credits.to_f / total_credits.to_f)
+        end
+        return total_score
+    end
 end
