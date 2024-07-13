3b3c89b7606d8c15cb5c15512351f08debb86dcf
daniithompson <danii.thompson8@gmail.com>
2024-07-13 16:07:09 +0100

    more validation to years model

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,4 +1,6 @@
 class Year < ApplicationRecord
   belongs_to :user
   has_many :course_modules, dependent: :destroy
+
+  validates :year_number, presence: true, numericality: { only_integer: true}, comparison: { greater_than: 0}, uniqueness: { scope: :user_id }
 end
