c84bb8706d2868abd5536b640a4d24cc978192fe
daniithompson <danii.thompson8@gmail.com>
2025-07-06 01:58:01 +0100

    fixed webpack configuration force all imports of bootstrap to use the same path (avoid importing it twice)

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,8 +1,15 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig, merge } = require('shakapacker')
+const path = require('path')
+
 const options = {
   resolve: {
-    extensions: ['.css', '.scss']
+    extensions: ['.css', '.scss'],
+    alias: {
+      // force webpack to use the same instance of bootstrap to avoid duplicate imports
+      'bootstrap': path.resolve(__dirname, '../../node_modules/bootstrap')
+    }
   }
 }
+
 module.exports = merge({}, generateWebpackConfig(), options)
